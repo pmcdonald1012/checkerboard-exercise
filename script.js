631c737b3754eb0053376ac445be8596fb3f9b30
Paul McDonald <pmcdonald1012@gmail.com>
2022-06-23 17:55:23 -0400

    finished project

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ document.body.append(container);
 
 for (let i = 0; i <= 8; i++) {
     for (let j = 0; j < 8; j++) {
+        //create tile
         var div = document.createElement('div');
+        //set tile dimensions and color
         div.style.width = '11.1%';
         div.style.float = 'left'
         div.style.paddingBottom =  "11.1%"
         div.style.border = '1px black solid'
+        //makes tile black or red
         if (j % 2 === 1 && i % 2 === 0) {
             div.style.background = "red";
         } else if (j % 2 === 0 && i % 2 === 1) {
